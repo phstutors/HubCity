c05474b15bf3368517716b183ad2955e6a2cc5e1
PhsTutors <phs3@discente.ifpe.edu.br>
2024-04-22 16:39:08 -0300

    Configura as telas

--- pages/Home.js
+++ pages/Home.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, TextInput, Text, FlatList, Button, TouchableOpacity, Modal } from 'react-native';
+import { StyleSheet, View, TextInput, Text, Image, FlatList, Button, TouchableOpacity, Modal } from 'react-native';
 import TopBar from '../components/TopBar';
 import axios from 'axios';
 
@@ -12,6 +12,15 @@ const Home = () => {
   const [page, setPage] = useState(1);
   const [selectedCity, setSelectedCity] = useState(null); // Estado para controlar a cidade selecionada
   const [modalVisible, setModalVisible] = useState(false); // Estado para controlar a visibilidade do modal
+  const [cityImage, setCityImage] = useState(null);
+  const [mayorName, setMayorName] = useState('');
+const [historyCity, setHistoryCity] = useState('');
+  useEffect(() => {
+    if (selectedCity) {
+      fetchCityImage(selectedCity.municipio.id);
+      fetchMayorName(selectedCity.municipio.id);
+    }
+  }, [selectedCity]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -44,17 +53,55 @@ const Home = () => {
   const handleSearch = (text) => {
     setSearch(text);
   };
-
-  const handleCityPress = (city) => {
-    setSelectedCity(city); // Define a cidade selecionada
-    setModalVisible(true); // Exibe o modal quando uma cidade é selecionada
+  
+  
+  const handleCityPress = async  (city) => {
+    await setSelectedCity(city);
+    setModalVisible(true); 
+    fetchCityHistory(city.id);
+    }
+  const fetchCityImage = async (cityId) => {
+    try {
+      const response = await axios.get(`https://servicodados.ibge.gov.br/api/v1/biblioteca?codmun=${cityId}&aspas=3&fotografias=1`);
+      const result = response.data;
+      const firstObject = Object.values(result)[0];
+      const image = `https://servicodados.ibge.gov.br/api/v1/resize/image?maxwidth=600&maxheight=600&caminho=biblioteca.ibge.gov.br/visualizacao/fotografias/GEBIS%20-%20RJ/${firstObject.LINK}`
+      setCityImage(image)
+    } catch (error) {
+    }
   };
-
+  const fetchCityHistory = async (cityId) => {
+    try {
+      const response = await axios.get(`https://servicodados.ibge.gov.br/api/v1/localidades/distritos/${cityId}`);
+      const history = response.data.historico; // Obtendo o histórico do distrito
+      console.log(history); // Fazendo algo com o histórico, como imprimir no console
+      setHistoryCity(history)
+    } catch (error) {
+      console.error('Error fetching city history:', error);
+    }
+  };
+  const fetchMayorName = async (cityId) => {
+    try {
+        const response = await axios.get(`https://servicodados.ibge.gov.br/api/v1/pesquisas/indicadores/29170%7C97907%7C97911/resultados/${cityId}`);
+        const data = response.data;
+        let mayorName = '';
+        data.forEach((item) => {
+          item.res.forEach((resItem) => {
+            if (resItem.res['2021']) {
+              mayorName = resItem.res['2021'];
+            }
+          });
+      });
+    setMayorName(mayorName);
+    } catch (error) {
+      console.error('Error fetching mayor name:', error);
+    }
+  };
   const renderCity = ({ item }) => (
     // Adicionando um TouchableOpacity para lidar com o clique em uma cidade
     <TouchableOpacity onPress={() => handleCityPress(item)} style={styles.containerCityCard}>
-      <View>
-      <Text style={styles.containerCityCardText}>Nome: {item.nome}</Text>
+      <View style={styles.cards}>
+      <Text style={styles.containerCityCardText}>Nome: {item.municipio.nome} UF: {item.municipio.microrregiao.mesorregiao.UF.sigla}</Text>
       <Text style={styles.containerCityCardText}>Região: {item.municipio.microrregiao.mesorregiao.UF.regiao.nome}</Text>
       </View>
     </TouchableOpacity>
@@ -78,7 +125,7 @@ const Home = () => {
         data={filteredCities.slice((page - 1) * 10, page * 10)}
         renderItem={renderCity}
         keyExtractor={(item, index) => index.toString()}
-        numColumns={2}
+       
       />
       <View style={styles.pagination}>
         <Button title="Anterior" disabled={page === 1} onPress={handlePrevPage} />
@@ -95,15 +142,25 @@ const Home = () => {
     <View style={styles.modalContainer}>
       {selectedCity && ( // Verifica se selectedCity não é nulo
         <View>
-          <Text style={styles.cityName}>{selectedCity.nome}</Text>
+          <Text style={styles.cityName}>{selectedCity.municipio.nome}</Text>
           <Text style={styles.cityInfo}>ID: {selectedCity.id}</Text>
           <Text style={styles.cityInfo}>Nome: {selectedCity.nome}</Text>
-          <Text style={styles.cityInfo}>Município: {selectedCity.municipio?.nome}</Text>
+          <Text style={styles.cityInfo}>Município: {selectedCity.municipio.historico}</Text>
+          <Text style={styles.cityInfo}>Prefeito: {mayorName}</Text>
+          <Text style={styles.cityInfo}>Prefeito: {historyCity}</Text>
           <Text style={styles.cityInfo}>Microrregião: {selectedCity.municipio?.microrregiao?.nome}</Text>
           <Text style={styles.cityInfo}>Mesorregião: {selectedCity.municipio?.microrregiao?.mesorregiao?.nome}</Text>
           <Text style={styles.cityInfo}>UF: {selectedCity.municipio?.microrregiao?.mesorregiao?.UF?.nome}</Text>
           <Text style={styles.cityInfo}>Região: {selectedCity.municipio?.microrregiao?.mesorregiao?.UF?.regiao?.nome}</Text>
-          <Button title="Fechar" onPress={() => setModalVisible(false)} />
+          {cityImage !== null ? (
+  <Image source={{ uri: cityImage }} style={{ width: 300, height: 300 }} />
+) : (
+  <Text style={{ color: '#fff' }}>Imagem não disponível</Text>
+)}
+          <Button title="Fechar" onPress={() => {
+  setModalVisible(false);
+  setCityImage(null);
+}} />
         </View>
       )}
     </View>
@@ -115,7 +172,7 @@ const Home = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
+    padding: 10,
     backgroundColor: '#18163D',
   },
 
@@ -131,12 +188,12 @@ const styles = StyleSheet.create({
   },
   containerCityCard:{
     backgroundColor: '#2A276B',
-    minWidth: '45%',
-    height: 43,
+    maxWidth: '100%',
+    height: 36,
     borderRadius: 14,
     margin: '2.5%',
-    justifyContent: 'center',
-    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'column'
   },
   cityInfo:{
     textAlign: 'justify',
